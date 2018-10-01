4834b3b07349565fb79bf76051af0fb0a0c53b21
Emma Gilmour <e.gilmour33@gmail.com>
2018-10-01 08:14:31 +0100

    changed DB_URL require

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const express = require('express');
 const apiRouter = require('./routes/api')
-const DB_URL = require('./config/config.js');
+const { DB_URL } = process.env.NODE_ENV === 'production' ? process.env : require('./config/config.js');
 const bodyParser = require('body-parser');
 const { handle404s, handle400s, handle500s } = require('./errors');
 const app = express();
